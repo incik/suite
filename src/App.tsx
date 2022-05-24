27f527fd626455bc6257fb5a1023ab9be3480306
Tomas Vaisar <tomas@vaisar.cz>
2022-05-24 12:51:24 +0200

    initital commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-slate-200">
+      <div className=" container mx-auto">
+        <div className="bg-slate-600 text-white rounded-b-xl border-t-2 border-red-400">
+          <div className="flex p-4 items-end">
+            <div className="flex flex-col items-start">
+              <h1 className="text-3xl font-bold">SUITE</h1>
+              <em className="text-xs">
+                Shoptet's Ultimate Interactive Template Editor
+              </em>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
